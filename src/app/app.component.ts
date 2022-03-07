ac86bcd9593c97206903c6da040c288e0d25e7b8
Lawrence Gumbo <84715458+lawrence-sert@users.noreply.github.com>
2022-03-07 17:24:47 +0200

    was park-web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
+import { CanonicalService } from './services/canonical.service';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +9,45 @@ import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'park-web';
 
   constructor(
-
     public router: Router,  
-    private activatedRoute: ActivatedRoute,  
-   
+    private activatedRoute: ActivatedRoute,
+    private metaTagService: Meta,
+    private canonicalService: CanonicalService
     ) { 
 
   }
 
   ngOnInit() { 
- 
-    } 
+    this.metaTagService.addTags([
+      { name: 'keywords', content: 'Angular SEO Integration, Music CRUD, Angular Universal' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Lawrence Gumbo' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2022-02-17', scheme: 'YYYY-MM-DD' },
+      { property: 'og:locale', content: 'en_US' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:title', content: 'SERT' },
+      { property: 'og:description', content: 'SERT is a full on-line service company that provides our clients with web' },
+      { property: 'og:url', content: 'https://www.sert.rw/' },
+      { property: 'og:site_name', content: 'SERT' },
+      { property: 'og:image', content: 'https://www.sert.rw/img/seo/sert-logo-300x300.png' },
+      { property: 'og:image:width', content: '300' },
+      { property: 'og:image:height', content: '300' },
+      { name: 'twitter:card', content:'summary' },
+      { name: 'twitter:description', content:'twitter description' },
+      { name: 'twitter:creator', content:'@sertrw1' },
+      { name: 'twitter:url', content:'https://www.sert.rw/' },
+      { name: 'twitter:image', content: 'https://www.sert.rw/img/twitter_image.png' },
+
+      { charset: 'UTF-8' },
+
+      ]);
+
+    this.canonicalService.setCanonicalURL();
+  } 
 }
+
+
+
--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private toastr: ToastrService) { }
+	title = 'Cook Africa';
 
-  ngOnInit(): void {
-  	this.toastr.success('Hello world!', 'Toastr fun!');
-  }
+	constructor(
+		private toastr: ToastrService,
+		private titleService: Title,
+		private metaTagService: Meta
+
+		) { }
+
+	ngOnInit(): void {
+		//set title for every page
+		this.titleService.setTitle(this.title);
+		this.metaTagService.updateTag(
+			{ name: 'description', content: 'Add song template' }
+			);
+		// this.toastr.success('Hello world!', 'Toastr fun!');
+	}
 
 }
